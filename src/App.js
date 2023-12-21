a5da9ab6ba345a1133408531dfdd71f0baa8808f
risingsunomi <vincentcastro@gmail.com>
2023-12-20 21:24:04 -0500

    removing cors fix

--- src/App.js
+++ src/App.js
@@ -85,8 +85,7 @@ function App() {
                     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
                     credentials: "same-origin", // include, *same-origin, omit
                     headers: {
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*"
+                        "Content-Type": "application/json"
                     },
                     redirect: "follow", // manual, *follow, error
                     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
