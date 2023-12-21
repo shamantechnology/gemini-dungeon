2ad7632d02e54506a396c6d0b8c9aafe40c677cd
risingsunomi <vincentcastro@gmail.com>
2023-12-20 21:59:20 -0500

    origin again

--- src/App.js
+++ src/App.js
@@ -85,7 +85,8 @@ function App() {
                     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
                     credentials: "same-origin", // include, *same-origin, omit
                     headers: {
-                        "Content-Type": "application/json"
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*"
                     },
                     redirect: "follow", // manual, *follow, error
                     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
