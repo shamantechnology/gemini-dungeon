3026744fd7ff242d2e0970c98014256dc514cbae
risingsunomi <vincentcastro@gmail.com>
2024-01-01 16:16:19 -0500

    trying to fix allow method

--- src/App.js
+++ src/App.js
@@ -89,7 +89,8 @@ function App() {
                     credentials: "same-origin", // include, *same-origin, omit
                     headers: {
                         "Content-Type": "application/json",
-                        "Access-Control-Allow-Origin": "*"
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE"
                     },
                     redirect: "follow", // manual, *follow, error
                     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
@@ -140,7 +141,8 @@ function App() {
                         credentials: "same-origin", // include, *same-origin, omit
                         headers: {
                             "Content-Type": "application/json",
-                            "Access-Control-Allow-Origin": "*"
+                            "Access-Control-Allow-Origin": "*",
+                            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE"
                         },
                         redirect: "follow", // manual, *follow, error
                         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
