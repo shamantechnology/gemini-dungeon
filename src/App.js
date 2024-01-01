5787fe6c0f9f4d1a9b95b394e01abdf9340e0147
risingsunomi <vincentcastro@gmail.com>
2024-01-01 14:47:32 -0500

    fixing CORS issues

--- src/App.js
+++ src/App.js
@@ -158,6 +158,7 @@ function App() {
                     credentials: "same-origin", // include, *same-origin, omit
                     headers: {
                         "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*"
                     },
                     redirect: "follow", // manual, *follow, error
                     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
